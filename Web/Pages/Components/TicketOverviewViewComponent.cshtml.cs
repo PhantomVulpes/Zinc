b36a56b535c4b775de0ba262ab9b74517c4efced
PhantomVulpes <noahgbuss@gmail.com>
2024-10-05 22:11:13 -0500

    Removed useless space

--- Web/Pages/Components/TicketOverviewViewComponent.cshtml.cs
+++ Web/Pages/Components/TicketOverviewViewComponent.cshtml.cs
@@ -7,7 +7,6 @@ namespace Vulpes.Zinc.Web.Pages.Components;
 public record TicketOverviewModel(Project Project, Ticket Ticket);
 public class TicketOverviewViewComponent : ViewComponent
 {
-
     private readonly IDataRepository<Project> projectRepository;
 
     public TicketOverviewViewComponent(IDataRepository<Project> projectRepository)
