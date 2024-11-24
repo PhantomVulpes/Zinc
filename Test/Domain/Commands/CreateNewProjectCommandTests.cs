a788ab1d1c918a29ea213a61c6067d401a9e30a2
PhantomVulpes <noahgbuss@gmail.com>
2024-11-24 17:49:04 -0600

    Added project creation tests

---
+++ Test/Domain/Commands/CreateNewProjectCommandTests.cs
@@ -0,0 +1,41 @@
+﻿using Vulpes.Electrum.Core.Domain.Exceptions;
+using Vulpes.Zinc.Domain.Commands;
+using Vulpes.Zinc.Domain.Models;
+using Vulpes.Zinc.Test.Doubles.Domain.Data;
+
+namespace Vulpes.Zinc.Test.Domain.Commands;
+
+[TestClass]
+public class CreateNewProjectCommandTests
+{
+    private readonly TestDataRepository<Project> testProjectRepository;
+
+    private readonly CreateNewProjectCommandHandler underTest;
+
+    public CreateNewProjectCommandTests()
+    {
+        testProjectRepository = new();
+
+        underTest = new(testProjectRepository);
+    }
+
+    private static CreateNewProjectCommand Command() => new("Test Project", "TEST", "Test Project Description", [], Guid.NewGuid());
+
+    [TestMethod]
+    public async Task ProjectIsInserted()
+    {
+        await underTest.ExecuteAsync(Command());
+
+        Assert.IsTrue(testProjectRepository.Inserted.Any(), "Project was not inserted into the repository.");
+    }
+
+    [TestMethod, ExpectedException(typeof(ElectrumValidationException))]
+    public async Task ProjectIsValidated()
+    {
+        // Projects require a four-digit shorthand.
+        var command = Command() with { ProjectShorthand = "TESTING" };
+        await underTest.ExecuteAsync(command);
+
+        Assert.Fail("Project was not validated.");
+    }
+}
