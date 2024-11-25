563b029b33ec911f66fc9a189430b7b466d3797a
PhantomVulpes <noahgbuss@gmail.com>
2024-11-24 18:21:42 -0600

    Added tests for deleting tickets

---
+++ Test/Domain/Commands/DeleteTicketFromDatabaseCommandTests.cs
@@ -0,0 +1,61 @@
+﻿using Vulpes.Zinc.Domain.Commands;
+using Vulpes.Zinc.Domain.Models;
+using Vulpes.Zinc.Test.Doubles;
+using Vulpes.Zinc.Test.Doubles.Domain.Data;
+
+namespace Vulpes.Zinc.Test.Domain.Commands;
+
+[TestClass]
+public class DeleteTicketFromDatabaseCommandTests
+{
+    private readonly TestDataRepository<Ticket> ticketRepository;
+    private readonly TestDataRepository<Project> projectRepository;
+
+    private readonly DeleteTicketFromDatabaseCommandHandler underTest;
+
+    public DeleteTicketFromDatabaseCommandTests()
+    {
+        (ticketRepository, projectRepository) = TestHelper.CreateTestDataRepositories();
+
+        underTest = new DeleteTicketFromDatabaseCommandHandler(ticketRepository, projectRepository);
+    }
+
+    public static DeleteTicketFromDatabaseCommand Command(Guid ticketKey, Guid userKey) => new(ticketKey, userKey);
+
+    [TestMethod]
+    public async Task TicketIsDeleted()
+    {
+        await underTest.ExecuteAsync(Command(ticketRepository.Models.First().Key, ticketRepository.PreparedUser.Key));
+
+        Assert.IsTrue(ticketRepository.Deleted.Any(), "Ticket was not deleted.");
+    }
+
+    [TestMethod]
+    public async Task TicketCannotBeDeletedWithSpecifiedStatus()
+    {
+        foreach (var status in Enum.GetValues<TicketStatus>())
+        {
+            var updatedTicket = ticketRepository.Models.First().Value with { Status = status };
+            ticketRepository.Models[updatedTicket.Key] = updatedTicket;
+
+            var result = await underTest.ValidateAccessAsync(Command(updatedTicket.Key, ticketRepository.PreparedUser.Key));
+
+            if (status == TicketStatus.Cancelled)
+            {
+                Assert.IsTrue(result, $"Ticket with status {status} was not allowed to be deleted.");
+            }
+            else
+            {
+                Assert.IsFalse(result, $"Ticket with status {status} was allowed to be deleted.");
+            }
+        }
+    }
+
+    [TestMethod]
+    public async Task InvalidUserCannotDeleteTicket()
+    {
+        var result = await underTest.ValidateAccessAsync(Command(ticketRepository.Models.First().Key, Guid.NewGuid()));
+
+        Assert.IsFalse(result, "Invalid user was allowed to delete ticket.");
+    }
+}
